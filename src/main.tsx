27193ecf2c59203f15502dd8b0b0441e1ae50fc7
kalai <kalaiselvant0@gmail.com>
2026-09-02 19:21:23 +0200

    feat: premium dark theme tokens and base chrome

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,10 @@
+import '@fontsource/literata/400.css';
+import '@fontsource/literata/600.css';
+import '@fontsource/newsreader/400.css';
+import '@fontsource/newsreader/600.css';
+import './styles/tokens.css';
+import './styles/global.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
